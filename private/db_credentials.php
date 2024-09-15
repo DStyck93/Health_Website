b1e8f863286c00acc7f681914bf9728b4114269e
DStyck93 <dstyck93@gmail.com>
2024-09-15 16:36:05 -0500

    Updated DB credentials and permissions.

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 const DB_SERVER = "localhost";
-const DB_USER = "root";
-const DB_PASS = "";
+const DB_USER = "DStyck";
+const DB_PASS = "k7A85\$RH#T!9%3b@wn9$";
 const DB_NAME = "health_website";
 ?>

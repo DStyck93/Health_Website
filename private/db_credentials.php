682cdb4662a24b12295be147431cf795613117f1
Dakota <76007168+DStyck93@users.noreply.github.com>
2024-09-18 16:12:23 -0500

    Delete private/db_credentials.php

--- private/db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-const DB_SERVER = "health-app.choeccos4euz.us-east-2.rds.amazonaws.com";
-const DB_ADMIN = "DStyck";
-const DB_ADMIN_PASS= "YTy!32E13#ZEA1&DTWn!";
-const DB_NAME = "health_website";
-//const DB_USER = "webuser";
-//const DB_USER_PASS = "!Y#PUDTao!?fxcU@HLf?"
-?>
